29d7e1043c570e68009648bc00ab3eb07d684908
sumssumm <sumssum.p@gmail.com>
2024-08-04 01:41:39 +0900

    8/4

--- class2/2775.cpp
+++ class2/2775.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
   int T;
   cin >> T;
 
-  int sum = 0;
-  int k, n;
-  for (int i = 0; i < T; ++i) {
-    cin >> k;
-    cin >> n;
-    for (int i = 0; i <= n; ++i) sum += i;
-    cout << sum << "\n";
+  vector<vector<int> > rooms(15, vector<int>(15, 0));
+  for (int i = 1; i < 15; ++i) rooms[0][i] = i;
+
+  for (int j = 1; j < 15; ++j) {
+    for (int k = 1; k < 15; ++k)
+      rooms[j][k] = rooms[j][k - 1] + rooms[j - 1][k];
+  }
+
+  while (T--) {
+    int k, n;
+    cin >> k >> n;
+    cout << rooms[k][n] << "\n";
   }
   return 0;
 }
