52482b1f09bc3484547dcf2be9496c50bb698927
sumssumm <sumssum.p@gmail.com>
2025-02-02 23:38:04 +0900

    stack

--- class3/2579.cpp
+++ class3/2579.cpp
@@ -1,3 +1,26 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
-using namepsace std;
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+
+  int score[301] = {0};
+  int dp[301] = {0};
+
+  for (int i = 1; i <= n; i++) cin >> score[i];
+
+  dp[1] = score[1];
+  if (n >= 2) dp[2] = score[1] + score[2];
+  if (n >= 3) dp[3] = max(score[1] + score[3], score[2] + score[3]);
+
+  for (int i = 4; i <= n; i++) {
+    dp[i] = max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i]);
+  }
+
+  cout << dp[n] << "\n";
+  return 0;
+}
